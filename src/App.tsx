a00e1e38c6f7834f1d01ff399cb99e249caeeefe
Chaim Bechor <hbx5555@gmail.com>
2026-03-25 17:47:22 +0200

    Revert to Store icon - trash icon moved to TrashMarket project

--- src/App.tsx
+++ src/App.tsx
@@ -745,7 +745,7 @@ function MarketplaceApp() {
 
             <div className="relative z-10 flex-1 flex flex-col items-center pt-20 px-8 h-full">
               <div className="w-40 h-40 bg-success rounded-3xl flex items-center justify-center shadow-xl mb-6">
-                <img src="/new_svg_trash_icon.svg" alt="TrashMarket" className="w-[90px] h-[90px]" />
+                <Store className="text-zinc-900 w-20 h-20" />
               </div>
               <h1 className="text-white text-5xl font-extrabold tracking-tight mb-2">מרקטפלייס</h1>
               <p className="text-white/90 text-xl font-medium mb-12">קניה ומכירה לכולם</p>
